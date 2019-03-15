8f2ea00af2595f15359dba3658b20dcc0021fe0f
Giuliano Andronic <giuliano.andronic@studenti.unitn.it>
2019-03-15 17:56:30 +0100

    display basic navigation on app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { NavBar } from './components/navigation/navbar';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {image: <img class="face" alt="profile image" src="https://placekitten.com/100/100"></img>,name:"bobinsky", surname:"smith"},
+      nav_list: [],
+      menu_list: [{id: 3, content: 'option1'}, {id: 4, content: 'option2'}, {id:5, content: 'option3'}, {id:6, content: 'option4'}]
+    };
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <NavBar user_elements={this.state.user} nav_elements={this.state.nav_list} menu_elements={this.state.menu_list}></NavBar>
     );
   }
 }
