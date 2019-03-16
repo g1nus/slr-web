afa14cef6652a12ea0f6d55cd89e925c6e5ae5cd
Giuliano Andronic <giuliano.andronic@studenti.unitn.it>
2019-03-16 16:11:53 +0100

    update app menu with dummy links

--- src/App.js
+++ src/App.js
@@ -9,15 +9,18 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      user: {image: <img class="face" alt="profile image" src="https://placekitten.com/100/100"></img>,name:"bobinsky", surname:"smith"},
+      user: {image: <img class="face" alt="profile image" src="https://placekitten.com/100/100"></img>,name:"Bobinsky", surname:"Sylvester"},
       nav_list: [],
-      menu_list: [{id: 3, content: 'option1'}, {id: 4, content: 'option2'}, {id:5, content: 'option3'}, {id:6, content: 'option4'}],
+      menu_list: [{id:3, content: <a className="menu-option">option1</a>}, 
+                  {id:4, content: <a className="menu-option">option2</a>}, 
+                  {id:5, content: <a className="menu-option">option3</a>}, 
+                  {id:6, content: <a className="menu-option">option4</a>}],
       main: <LoadIcon></LoadIcon>
     };
   }
   render() {
     return (
-      <div class="body-wrapper">
+      <div className="app">
         <NavBar user_elements={this.state.user} nav_elements={this.state.nav_list} menu_elements={this.state.menu_list}></NavBar>
         <Main main_element={this.state.main}></Main>
       </div>
