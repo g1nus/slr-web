822a98491fac4ba9493b8aa7e084b6f5bf84ec68
Giuliano Andronic <giuliano.andronic@studenti.unitn.it>
2019-03-17 18:46:54 +0100

    improve events handling

--- src/App.js
+++ src/App.js
@@ -1,27 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './svg/logo.svg';
 import './App.css';
-import { NavBar } from './components/navigation/navbar';
-import { LoadIcon } from './components/loadicon';
-import { Main } from './components/main';
+import LoadIcon from './components/loadicon';
+import SearchForm from './components/forms/searchform';
+import NavBar from './components/navigation/navbar';
+import Main from './components/main';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      user: {image: <img class="face" alt="profile image" src="https://placekitten.com/100/100"></img>,name:"Bobinsky", surname:"Sylvester"},
-      nav_list: [],
-      menu_list: [{id:3, content: <a className="menu-option">option1</a>}, 
-                  {id:4, content: <a className="menu-option">option2</a>}, 
-                  {id:5, content: <a className="menu-option">option3</a>}, 
-                  {id:6, content: <a className="menu-option">option4</a>}],
+  state = {
+      user: {image: <img className="face" alt="profile" src="https://placekitten.com/100/100"></img>,name:"Bobinsky", surname:"Sylvester"},
+      nav_list: [{id:1, content: "PAGE 1", link: "pag1", selected: "selected"},
+                 {id:2, content: "PAGE 2", link: "pag2", selected: ""}],
+      menu_list: [{id:3, content: "option1", link: "search"}, 
+                  {id:4, content: "option2", link: ""}, 
+                  {id:5, content: "option3", link: ""}, 
+                  {id:6, content: "option4", link: ""}],
       main: <LoadIcon></LoadIcon>
-    };
+  };
+
+  handleNavLinkClick = link => {
+    this.setState(prevState => {
+      const nav_list = prevState.nav_list.map((item) => {
+        let newitem = item;
+        if (item.link === link) {
+          newitem = item;
+          newitem.selected = "selected";
+        } else {
+          newitem = item;
+          newitem.selected = "";
+        }
+        return newitem;
+      });
+      return {
+        main: <LoadIcon></LoadIcon>,
+        nav_list,
+      };
+    });
   }
+
+  handleMenuLinkClick = link => {
+    if(link === "search"){
+      this.setState({main: <SearchForm></SearchForm>});
+    }else{
+      this.setState({main: <LoadIcon></LoadIcon>});
+    }
+  }
+  
   render() {
     return (
       <div className="app">
-        <NavBar user_elements={this.state.user} nav_elements={this.state.nav_list} menu_elements={this.state.menu_list}></NavBar>
+        <NavBar navhandler={this.handleNavLinkClick} menuhandler={this.handleMenuLinkClick} user_elements={this.state.user} nav_elements={this.state.nav_list} menu_elements={this.state.menu_list} underline={15}></NavBar>
         <Main main_element={this.state.main}></Main>
       </div>
     );
