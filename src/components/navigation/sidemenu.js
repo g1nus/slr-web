6f4c74112f58641603f7ba250ba4b1357b2cebc8
Giuliano Andronic <giuliano.andronic@studenti.unitn.it>
2019-03-17 18:44:38 +0100

    improve 'side menu' component

--- src/components/navigation/sidemenu.js
+++ src/components/navigation/sidemenu.js
@@ -1,73 +1,75 @@
-import React from "react";
+import React, {Component} from "react";
 
-class UserInfo extends React.Component{
-    render(){
-        return (
-            <div className="user" >
-                {this.props.user.image}
-                <div className="user-info">
-                    {this.props.user.name}
-                    <br/>
-                    {this.props.user.surname}
-                </div>
-            </div>
-        );
+const UserInfo = ({user}) => (
+  <div className="user" >
+    {user.image}
+    <div className="user-info">
+      {user.name}
+      <br/>
+      {user.surname}
+    </div>
+  </div>
+);
+
+class SideMenu extends Component{
+  state = {
+    shown: false, 
+    firsttime: true
+  };
+
+  handleLink = e => {
+    this.setState(prevstate => ({
+      shown: !prevstate.shown
+    }));
+    this.props.handler(e.target.getAttribute("data"));
+  }
+
+  handleToggleMenuButton = () =>{
+    this.setState(prevstate => ({
+      shown: !prevstate.shown,
+      firsttime: false,
+      menufocus: !prevstate.focus
+    }));
+  }
+
+  handleMenuBlur = () =>{
+    this.setState(prevstate => ({
+      shown: false
+    }));
+  }
+
+  render(){
+    var clsidemenu = "modal side-menu up";
+    var clsbutton = "button-wrapper close";
+    if(this.state.firsttime){
+      clsidemenu="modal side-menu"
     }
+    if(this.state.shown){
+      clsbutton="button-wrapper open"
+      clsidemenu="modal side-menu down"
+    }
+    return (
+      <div className="menu">
+        <div className={clsbutton} onClick={this.handleToggleMenuButton} onBlur={this.handleMenuBlur} tabIndex={-1}>
+          <svg id="menu-button" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+            viewBox="0 0 1000 1000">
+            <path className="st0" d="M214,482.1"/>
+            <line className="st-t" x1="172" y1="482.1" x2="172" y2="482.1"/>
+            <line className="st-m" x1="500" y1="482.1" x2="500" y2="482.1"/>
+            <line className="st-b" x1="828" y1="482.1" x2="828" y2="482.1"/>
+          </svg>
+      </div>
+      <div className={clsidemenu}>
+        <UserInfo user={this.props.user}></UserInfo>
+        {this.props.menu_elements.map((element, index) =>
+          <div key={index}>
+          <a href='#' data={element.link} className="menu-option" onMouseDown={this.handleLink}>{element.content}</a>
+          </div>
+        )}
+        </div>
+      </div>
+    );
+  }
 }
 
-class Menu extends React.Component{
-    render(){
-        var cls = "modal side-menu up";
-        if(this.props.show){
-            cls="modal side-menu down"
-        }
-        if(this.props.firsttime){
-            cls="modal side-menu"
-        }
-        return (
-            <div className={cls} >
-                <UserInfo user={this.props.user}></UserInfo>
-                {this.props.elements.map((element) =>
-                    <div key={element.id}>
-                    {element.content}
-                    </div>
-                )}
-            </div>
-        );
-    }
-}
-
-export class SideMenu extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {shown: false, firsttime: true};
-        this.handleToggleClick = this.handleToggleClick.bind(this);
-    }
-
-    handleToggleClick(){
-        this.setState(prevstate => ({
-            shown: !prevstate.shown,
-            firsttime: false
-        }));
-    }
-
-    render(){
-        var cls = "button-wrapper close";
-        if(this.state.shown){
-            cls="button-wrapper open"
-        }
-        return (
-        <div className="menu">
-            <div className={cls} onClick={this.handleToggleClick}>
-                <svg id="menu-button" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                    viewBox="0 0 1000 1000" xmlSpace="preserve">
-                    <path class="st0" d="M214,482.1"/>
-                    <line class="st-t" x1="172" y1="482.1" x2="172" y2="482.1"/>
-                    <line class="st-m" x1="500" y1="482.1" x2="500" y2="482.1"/>
-                    <line class="st-b" x1="828" y1="482.1" x2="828" y2="482.1"/>
-                </svg>
-            </div>
-            <Menu firsttime={this.state.firsttime} show={this.state.shown} user={this.props.user} elements={this.props.menu_elements}></Menu>
-        </div>);
-    }
-}
+export default SideMenu;
