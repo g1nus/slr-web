43da84e69b9ef284dc778d57cfc08357af76345c
Giuliano Andronic <giuliano.andronic@studenti.unitn.it>
2019-03-24 12:43:22 +0100

    remove search context
    the 'project page' component will keep the search info in its state

--- src/components/forms/searchform.js
+++ src/components/forms/searchform.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from 'react-router-dom'
 import CheckBox from "./checkbox";
-import { SearchResultsConsumer } from "../../providers/SearchResultsProvider"
+import ClampLines from 'react-clamp-lines';
 
 //the papers will be fetched through an api
 const PAPERS = [{id:"1", title:"paper one", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc condimentum mauris tristique quam tincidunt, nec sodales mauris ornare. Nunc non sapien eu felis interdum vehicula. Donec lacinia scelerisque ullamcorper. Sed viverra a dolor vitae volutpat. Duis non est non ligula lobortis fermentum. Donec finibus diam est, eget aliquet eros pellentesque vel. Nulla sit amet purus neque. Fusce pulvinar lobortis felis, in laoreet massa sollicitudin vestibulum. In consectetur felis massa, at varius justo ultricies in. Curabitur egestas euismod justo, sit amet consectetur velit sagittis eu. Phasellus ornare in libero eget semper. Sed quis risus in nulla mattis vestibulum. "}, 
@@ -9,17 +9,7 @@ const PAPERS = [{id:"1", title:"paper one", description:"Lorem ipsum dolor sit a
                   {id:"3", title:"paper two", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc condimentum mauris tristique quam tincidunt, nec sodales mauris ornare. Nunc non sapien eu felis interdum vehicula. Donec lacinia scelerisque ullamcorper. Sed viverra a dolor vitae volutpat. Duis non est non ligula lobortis fermentum. Donec finibus diam est, eget aliquet eros pellentesque vel. Nulla sit amet purus neque. Fusce pulvinar lobortis felis, in laoreet massa sollicitudin vestibulum. In consectetur felis massa, at varius justo ultricies in. Curabitur egestas euismod justo, sit amet consectetur velit sagittis eu. Phasellus ornare in libero eget semper. Sed quis risus in nulla mattis vestibulum. "}];
 const OPTIONS1 = ["option one", "option two", "option three"];
 
-const  SearchForm = () => {
-  const [query, setQuery] = useState('');
-  const [checkboxes, setCheckboxes] = useState({
-    one: OPTIONS1.reduce(
-      (options, option) => ({
-        ...options,
-        [option]: false
-      }),
-      {}
-    )
-  });
+const  SearchForm = ({query, setQuery, checkboxes, setCheckboxes, results, setResults}) => {
 
   function handleCheckboxChange(e){
     const { name } = e.target;
@@ -34,50 +24,63 @@ const  SearchForm = () => {
 
   function createCheckboxes(){return OPTIONS1.map(option => {return <CheckBox label={option} isSelected={checkboxes.one[option]} handler={handleCheckboxChange} key={option} />})}
 
+  function updateSearchResults(papers){
+    console.log(papers)
+    setResults(papers)
+  }
+
   return(
-    <SearchResultsConsumer>
-    {( context ) => (
-    <div>
-      <form className={(context.results.length === 0) ? 'search-form' : 'search-form small' } onSubmit={(e) => {
+    <>
+      <form className={(results.length === 0) ? 'search-form' : 'search-form small' } onSubmit={(e) => {
         e.preventDefault();
-        context.updateSearchResults(PAPERS);
+        updateSearchResults(PAPERS);
         }}>
-        <input
-          type="text"
-          placeholder="search"
-          value={query}
-          onChange={e => setQuery(e.target.value)}
-        />
-        <div className="option-holder" style={{visibility: (context.results.length === 0) ? 'visible' : 'hidden' }}>
+        <div style={{position:'relative'}}>
+          <input
+            type="text"
+            placeholder="search"
+            value={query}
+            onChange={e => setQuery(e.target.value)}
+          />
+          <button type="submit" value="Submit" disabled={(query === '')}>
+            <svg id="search-icon" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+            viewBox="0 0 1000 1000">
+              <path className="st0" d="M626.9,644.3c-73.5,0-142.6-28.6-194.6-80.6s-80.6-121.1-80.6-194.6s28.6-142.6,80.6-194.6
+                c52-52,121.1-80.6,194.6-80.6c73.5,0,142.6,28.6,194.6,80.6c52,52,80.6,121.1,80.6,194.6s-28.6,142.6-80.6,194.6
+                S700.4,644.3,626.9,644.3z"/>
+              <line className="st1" x1="145.2" y1="850.8" x2="431.7" y2="564.3"/>
+          </svg>
+          </button>
+        </div>
+        <div className="option-holder">
           <label>Option:</label><br/>
           <div className="checkboxes-holder">
             {createCheckboxes()}
           </div>
         </div>
-        <button type="submit" value="Submit" disabled={(query === '')}>
-          <svg id="search-icon" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-          viewBox="0 0 1000 1000">
-            <path className="st0" d="M626.9,644.3c-73.5,0-142.6-28.6-194.6-80.6s-80.6-121.1-80.6-194.6s28.6-142.6,80.6-194.6
-              c52-52,121.1-80.6,194.6-80.6c73.5,0,142.6,28.6,194.6,80.6c52,52,80.6,121.1,80.6,194.6s-28.6,142.6-80.6,194.6
-              S700.4,644.3,626.9,644.3z"/>
-            <line className="st1" x1="145.2" y1="850.8" x2="431.7" y2="564.3"/>
-        </svg>
-        </button>
       </form>
+      <form className="search-results" style={{visibility: (results.length === 0) ? 'hidden' : '' }}>
+          {results.map((element, index) =>
+                <div key={index} className="paper-card">
+                <CheckBox label={""}/>
+                  <Link to={"#"}><h3>{element.title}</h3></Link>
+                  <ClampLines
+                    text={element.description}
+                    lines= {4}
+                    ellipsis="..."
+                    moreText="Expand"
+                    lessText="Collapse"
+                    className="paragraph"
+                    moreText={"more"}
+                    lessText={"less"}
+                  />
+                </div>
+              
+          )}
+          <button type="submit" value="Submit">+</button>
+        </form>
+    </>
 
-          <div className="search-results">
-            {context.results.map((element, index) =>
-                <Link key={index} to={"#"}>
-                  <div  className="light-modal paper-card">
-                    <h3>{element.title}</h3>
-                    <p>{element.description}</p>
-                  </div>
-                </Link>
-            )}
-        </div>
-    </div>
-    )}
-    </SearchResultsConsumer>
   );
 }
 
--- src/components/projectpage.js
+++ src/components/projectpage.js
@@ -2,16 +2,29 @@ import React, { useState, useEffect } from "react";
 import { Route, Link } from 'react-router-dom'
 import { Flipper, Flipped } from 'react-flip-toolkit';
 import SearchForm from '../components/forms/searchform';
-import { SearchResultsProvider } from "../providers/SearchResultsProvider"
+import ClampLines from 'react-clamp-lines';
 
 //the projects will be fetched through an api
 const PROJECT = {id:"1", name:"project one", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc condimentum mauris tristique quam tincidunt, nec sodales mauris ornare. Nunc non sapien eu felis interdum vehicula. Donec lacinia scelerisque ullamcorper. Sed viverra a dolor vitae volutpat. Duis non est non ligula lobortis fermentum. Donec finibus diam est, eget aliquet eros pellentesque vel. Nulla sit amet purus neque. Fusce pulvinar lobortis felis, in laoreet massa sollicitudin vestibulum. In consectetur felis massa, at varius justo ultricies in. Curabitur egestas euismod justo, sit amet consectetur velit sagittis eu. Phasellus ornare in libero eget semper. Sed quis risus in nulla mattis vestibulum. "};
 const PAPERS = [{id:"1", title:"paper one", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc condimentum mauris tristique quam tincidunt, nec sodales mauris ornare. Nunc non sapien eu felis interdum vehicula. Donec lacinia scelerisque ullamcorper. Sed viverra a dolor vitae volutpat. Duis non est non ligula lobortis fermentum. Donec finibus diam est, eget aliquet eros pellentesque vel. Nulla sit amet purus neque. Fusce pulvinar lobortis felis, in laoreet massa sollicitudin vestibulum. In consectetur felis massa, at varius justo ultricies in. Curabitur egestas euismod justo, sit amet consectetur velit sagittis eu. Phasellus ornare in libero eget semper. Sed quis risus in nulla mattis vestibulum. "}, 
                   {id:"2", title:"paper two", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc condimentum mauris tristique quam tincidunt, nec sodales mauris ornare. Nunc non sapien eu felis interdum vehicula. Donec lacinia scelerisque ullamcorper. Sed viverra a dolor vitae volutpat. Duis non est non ligula lobortis fermentum. Donec finibus diam est, eget aliquet eros pellentesque vel. Nulla sit amet purus neque. Fusce pulvinar lobortis felis, in laoreet massa sollicitudin vestibulum. In consectetur felis massa, at varius justo ultricies in. Curabitur egestas euismod justo, sit amet consectetur velit sagittis eu. Phasellus ornare in libero eget semper. Sed quis risus in nulla mattis vestibulum. "}, 
                   {id:"3", title:"paper two", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc condimentum mauris tristique quam tincidunt, nec sodales mauris ornare. Nunc non sapien eu felis interdum vehicula. Donec lacinia scelerisque ullamcorper. Sed viverra a dolor vitae volutpat. Duis non est non ligula lobortis fermentum. Donec finibus diam est, eget aliquet eros pellentesque vel. Nulla sit amet purus neque. Fusce pulvinar lobortis felis, in laoreet massa sollicitudin vestibulum. In consectetur felis massa, at varius justo ultricies in. Curabitur egestas euismod justo, sit amet consectetur velit sagittis eu. Phasellus ornare in libero eget semper. Sed quis risus in nulla mattis vestibulum. "}];
+const OPTIONS1 = ["option one", "option two", "option three"];
 
 const ProjectPage = ({ match }) => {
   const [slider, setSlider] = useState(true);
+  const [query, setQuery] = useState('');
+  const [checkboxes, setCheckboxes] = useState({
+    one: OPTIONS1.reduce(
+      (options, option) => ({
+        ...options,
+        [option]: false
+      }),
+      {}
+    )
+  });
+  const [results, setResults] = useState([])
+
   useEffect(() => {
     var substr = window.location.pathname.substring(window.location.pathname.length-7,window.location.pathname.length);
     if(substr === '/search' || substr === 'search/'){
@@ -20,8 +33,9 @@ const ProjectPage = ({ match }) => {
       setSlider(true);
     }
   });
+
   return(
-    <SearchResultsProvider><div  className="project-wrapper">
+    <div  className="project-wrapper">
       <div className="title">PROJECT NAME</div>
       <div className="project-nav-link-wrapper">
         <div className="nav-link">
@@ -36,20 +50,33 @@ const ProjectPage = ({ match }) => {
           </Flipped>
         </Flipper>
       </div>
-      <Route exact path = {match.url} render={() => 
+      <Route exact path = {match.url} render={() =>
+        <>
+        <div className="project-description">{PROJECT.description}</div>
         <div className="papers-list">
           {PAPERS.map((element, index) =>
-              <Link key={index} to={"#"}>
-                <div  className="light-modal paper-card">
-                  <h3>{element.title}</h3>
-                  <p>{element.description}</p>
+                <div key={index} className="paper-card">
+                  <Link to={"#"}><h3>{element.title}</h3></Link>
+                  <ClampLines
+                    text={element.description}
+                    lines= {4}
+                    ellipsis="..."
+                    moreText="Expand"
+                    lessText="Collapse"
+                    className="paragraph"
+                    moreText={"more"}
+                    lessText={"less"}
+                  />
                 </div>
-              </Link>
+              
           )}
         </div>
+        </>
       } />
-      <Route path = {match.url + "/search"} render={() => <SearchForm />} />
-    </div></SearchResultsProvider>        
+      <Route path = {match.url + "/search"} render={() => 
+        <SearchForm query={query} setQuery={setQuery} checkboxes={checkboxes} setCheckboxes={setCheckboxes} results={results} setResults={setResults}/>
+      } />
+    </div>
 
   );
 }
