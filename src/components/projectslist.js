294e8b8fe60e1a3c1e17d1f13f6686edd5027211
Giuliano Andronic <giuliano.andronic@studenti.unitn.it>
2019-03-24 15:57:03 +0100

    remove useless variable

--- src/components/projectslist.js
+++ src/components/projectslist.js
@@ -2,10 +2,6 @@ import React, { useState, useEffect } from "react";
 import { Link } from 'react-router-dom'
 import {projectsDao} from '../dao/projects.dao'
 import LoadIcon from '../components/loadicon';
-//the projects will be fetched through an api
-const PROJECTS = [{id:"1", name:"project one", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc condimentum mauris tristique quam tincidunt, nec sodales mauris ornare. Nunc non sapien eu felis interdum vehicula. Donec lacinia scelerisque ullamcorper. Sed viverra a dolor vitae volutpat. Duis non est non ligula lobortis fermentum. Donec finibus diam est, eget aliquet eros pellentesque vel. Nulla sit amet purus neque. Fusce pulvinar lobortis felis, in laoreet massa sollicitudin vestibulum. In consectetur felis massa, at varius justo ultricies in. Curabitur egestas euismod justo, sit amet consectetur velit sagittis eu. Phasellus ornare in libero eget semper. Sed quis risus in nulla mattis vestibulum. "}, 
-                  {id:"2", name:"project two", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc condimentum mauris tristique quam tincidunt, nec sodales mauris ornare. Nunc non sapien eu felis interdum vehicula. Donec lacinia scelerisque ullamcorper. Sed viverra a dolor vitae volutpat. Duis non est non ligula lobortis fermentum. Donec finibus diam est, eget aliquet eros pellentesque vel. Nulla sit amet purus neque. Fusce pulvinar lobortis felis, in laoreet massa sollicitudin vestibulum. In consectetur felis massa, at varius justo ultricies in. Curabitur egestas euismod justo, sit amet consectetur velit sagittis eu. Phasellus ornare in libero eget semper. Sed quis risus in nulla mattis vestibulum. "}, 
-                  {id:"3", name:"project three", description:""}];
 
 const ProjectsList = ({ match }) => {
   const [projectslist, setProjectsList] = useState([]);
