4311a631a96c19859a25e4b018f32f5300b085f7
Giuliano Andronic <giuliano.andronic@studenti.unitn.it>
2019-03-26 18:48:13 +0100

    Updates

---
+++ precache-manifest.3dea6e00a6ec0b88c049e809b57e115d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3002f0ae45ff693c7ba0",
+    "url": "/slr-web/static/css/main.85250957.chunk.css"
+  },
+  {
+    "revision": "3002f0ae45ff693c7ba0",
+    "url": "/slr-web/static/js/main.f0c0d435.chunk.js"
+  },
+  {
+    "revision": "728711b4df32723b671a",
+    "url": "/slr-web/static/js/runtime~main.204b5f6c.js"
+  },
+  {
+    "revision": "852630b3f95b01675efa",
+    "url": "/slr-web/static/js/2.97a0490c.chunk.js"
+  },
+  {
+    "revision": "df0b2afe312bf1e251cda115b8085f29",
+    "url": "/slr-web/static/media/tick.df0b2afe.svg"
+  },
+  {
+    "revision": "4876878306ddc6ac3c19d651ab8106a8",
+    "url": "/slr-web/index.html"
+  }
+];
